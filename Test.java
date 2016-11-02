9277c33e44d00b2d97276833df87f446d12120a0
xjssam <xjssam@126.com>
2016-11-02 15:37:42 +0800

    old

--- Test.java
+++ Test.java
@@ -1,11 +1,9 @@
-public class Test{
-	public void test(){
-	HT-XJS qwer1234
-	2016-11-01 15:42
-	A修改   分支b 改了东西  分支b 又改了东西
+package st;
+public class Test {
+	
+	
+	public static void old(String[] args) {
+		System.out.println("old");
 	}
-	fenzhi  分支 b-- 改了东西
-	2
+	
 }
-b1
-2016-11-02 b-- b
